ed1681c520008b21359a2de047b9552198b09fc1
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-15 09:52:51 +0100

    added routes for random user and all users

---
+++ server/api/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+
+// MIDDLEWARES
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+//Model import
+
+const User = require("../model/user");
+
+const allUsers = async (req, res) => {
+  console.log("Message data get block:");
+
+  try {
+    const data = await User.find({});
+    console.log("Users data is", data);
+    res.send(data);
+  } catch (error) {
+    console.log("Error is :", error.message);
+  }
+};
+
+// to get random user
+
+const randomUser = async (req, res) => {
+  console.log("rnadom user data get block:");
+
+  try {
+    const data = await User.aggregate([{ $sample: { size: 1 } }]);
+    console.log("Users data is", data);
+    res.send(data);
+  } catch (error) {
+    console.log("Error is :", error.message);
+  }
+};
+
+router.get("/users", allUsers);
+router.get("/me", randomUser);
+
+module.exports = router;
