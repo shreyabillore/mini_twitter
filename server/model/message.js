54fe4f396f567a5b372a567a7abee12b686d2a6b
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-03-10 19:17:07 +0100

    remove version key

--- server/model/message.js
+++ server/model/message.js
@@ -1,43 +1,40 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = mongoose.Schema(
-  {
-    text: {
+const MessageSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    maxlength: 144,
+    unique: false,
+  },
+  user: {
+    type: Object,
+    required: true,
+    username: {
       type: String,
       required: true,
-      maxlength: 144,
-      unique: false,
     },
-    user: {
-      type: Object,
-      required: true,
-      username: {
-        type: String,
-        required: true,
-      },
-      handle: {
-        type: String,
-        required: true,
-      },
-    },
-    date: {
-      required: true,
-      type: Date,
-      default: Date.now,
-    },
-    img_url: {
+    handle: {
       type: String,
-      required: false,
-      default:
-        "https://th.bing.com/th/id/OIP.sJhiNQZoqAiaGJusJ_M37wHaHa?pid=ImgDet&rs=1",
-    },
-    link_url: {
-      type: String,
-      required: false,
+      required: true,
     },
   },
-  { versionKey: "_twitter_message" }
-);
+  date: {
+    required: true,
+    type: Date,
+    default: Date.now,
+  },
+  img_url: {
+    type: String,
+    required: false,
+    default:
+      "https://th.bing.com/th/id/OIP.sJhiNQZoqAiaGJusJ_M37wHaHa?pid=ImgDet&rs=1",
+  },
+  link_url: {
+    type: String,
+    required: false,
+  },
+});
 
 const Message = mongoose.model("Message", MessageSchema);
 
